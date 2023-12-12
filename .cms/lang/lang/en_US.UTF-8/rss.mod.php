f68300c9f3a38dc1a37c8d19a3aa41fbf8b755ae
art-henry <65597700+art-henry@users.noreply.github.com>
2023-12-12 13:10:42 +0100

    Add files via upload

---
+++ .cms/lang/lang/en_US.UTF-8/rss.mod.php
@@ -0,0 +1,17 @@
+<?php
+$cms["lang"]["rss.mod.php"]["en_US.UTF-8"] = array(
+    "rss_module_name" => "RSS",
+    "rss_module_description" => "Setting up an RSS feed",
+    "view_rss" => "View RSS feed",
+    "excluded_links" => "Excluded links",
+    "save" => "Save",
+    "placeholder" => "Select page...",
+    "rss_gen" => "Rules for generating rss.rss file",
+    "static" => "Static",
+    "dynamic" => "Dynamic",
+    "update_freq" => "Frequency Recreate",
+    "static_freq" => "Static CRON generation at",
+    "minutes" => "-minute intervals",
+    "freq_help" => "This function works when CRON is on. Information on using CRON can be obtained from your hosting provider.",
+    "rss_items_count" => "Number of items in the RSS feed",
+);
---
+++ .cms/lang/ru_RU.UTF-8/template.mod.php
@@ -0,0 +1,19 @@
+<?php
+$cms["lang"]["template.mod.php"]["ru_RU.UTF-8"] = array(
+    "template_module_name" => "Шаблоны",
+    "template_module_description" => "Переключение шаблонов и управление их настройками",
+    "current_template" => "Текущий шаблон",
+    "save" => "Сохранить",
+    "editable_files" => "Редактируемые файлы этого шаблона",
+    "no_editable_files" => "Нет файлов для редактирования",
+    "confirm_save" => "Сохранить изменения?",
+    "saved" => "Сохранено",
+    "save_error" => "Ошибка сохранения",
+    "install_template" => "Установить шаблон",
+    "upload_error_xxx" => "Ошибка загрузки xxx",
+    "cant_unzip_xxx" => "Не могу распаковать xxx",
+    "install_success" => "Успешная установка",
+    "enable_cache" => "Включить кэш",
+    "cache_cleared" => "Кэш очищен",
+    "help" => "Справка",
+);
